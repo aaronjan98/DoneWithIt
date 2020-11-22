586853ef667ff7dfeab548a0ad09bdff1a4c7298
aaronjan <aaronjan98@gmail.com>
2020-11-22 15:42:12 -0800

    created close and delete icon buttons, really views just for now

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { StyleSheet, View, SafeAreaView } from 'react-native';
 import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 function App() {
   
   return ( 
-    <WelcomeScreen />
+    // <WelcomeScreen />
+    <ViewImageScreen />
   );
 }
 
